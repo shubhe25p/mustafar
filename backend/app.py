d4e83214a1f2281916dfa63a7cd4cd5f6a34d186
shuhbe25p <random@sp.com>
2023-01-21 17:09:25 -0800

    v2

--- backend/app.py
+++ backend/app.py
@@ -3,22 +3,28 @@ import os
 import warnings
 from PIL import Image
 from stability_sdk import client
-import cv2
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
-stability_api = client.StabilityInference(
-    key='
-    ', 
-    verbose=True,
-)
-answers = stability_api.generate(
-    prompt="houston, we are a 'go' for launch!"
-)
-for resp in answers:
-    for artifact in resp.artifacts:
-        if artifact.finish_reason == generation.FILTER:
-            warnings.warn(
-                "Your request activated the API's safety filters and could not be processed."
-                "Please modify the prompt and try again.")
-        if artifact.type == generation.ARTIFACT_IMAGE:
-            img = Image.open(io.BytesIO(artifact.binary))
-            img.save('output.png')
+
+
+def generate_img(city, planet):
+
+    stability_api = client.StabilityInference(
+        key='sk-vSTOSKxpVYvN54rEO8fGo4XXHiflxdedpipngg9dL8sqGznq', 
+        verbose=True,
+    )
+    answers = stability_api.generate(
+        prompt=manipulate_prompt(city,planet),
+    )
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again.")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                img.save('output.png')
+
+def manipulate_prompt(city, planet):
+    prompt = f"What would {city} look like on {planet}"
+    return prompt
--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 from flask_cors import CORS
+from app import generate_img
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 @app.route('/', methods=['POST'])
 def handle_post_request():
     data = request.get_json()
+    generate_img(data['city'], data['planet'])
     return data
     # call the python function
     # result = my_function(data)
